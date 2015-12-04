0d7345cff29abc01639d38bc70d3afd99ff1dd45
Xirath99 <bbrooks944@gmail.com>
2015-12-04 14:08:09 -0800

    Create DeclaringVariables.vb

---
+++ DeclaringVariables.vb
@@ -0,0 +1,37 @@
+Dim x As Integer
+Dim y As Integer
+
+x=7
+y=x+3
+
+Console.WritLine(y)
+
+Console.ReadLine()
+
+
+
+
+
+
+Dim myFirstName As String 
+
+myFirstName ="Bob"
+
+Console.Writeline(myFirstName)
+
+
+
+Dim myFirstName As String ="Bob"
+
+Dim myFirstName = "Bob" *still works*
+
+
+Dim x As Integer =7
+Dim y As String = "Bob"
+                           x.ToString() 
+Dim myFirstTry As String = CStr(x) & y
+Console.WriteLine(myFirstTry)
+
+
+Dim mySecondtry As Integer = x + CInt(y)
+Console. WriteLine(mySecondtry)
